259f6fd7f06713c8e5d466b7715f961309c4304d
Littlew <wei19960728@outlook.com>
2023-07-05 20:42:31 +0800

    add recycler view test

--- example/src/main/java/com/littlew/example/MainActivity.kt
+++ example/src/main/java/com/littlew/example/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.littlew.example
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import butterknife.BindView
 import butterknife.ButterKnife
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -14,6 +19,9 @@ class MainActivity : AppCompatActivity() {
 
     @BindView(R.id.textview)
     lateinit var textview: TextView
+
+    @BindView(R.id.recycler_view)
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -27,5 +35,31 @@ class MainActivity : AppCompatActivity() {
         fab.setOnClickListener { view ->
             textview.text = "Hello Android"
         }
+
+        val stringArray = this.resources.getStringArray(R.array.str_array)
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = RAdapter(stringArray.toList())
+        }
+    }
+
+    class RAdapter(private val list: List<String>) : RecyclerView.Adapter<RVH>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RVH {
+            return RVH(
+                LayoutInflater.from(parent.context)
+                    .inflate(R.layout.item_recycler_view, parent, false)
+            )
+        }
+
+        override fun getItemCount(): Int = list.size
+
+        override fun onBindViewHolder(holder: RVH, position: Int) {
+            holder.textView.text = list[position]
+        }
+
+    }
+
+    class RVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val textView: TextView by lazy { itemView.findViewById(R.id.item_text) }
     }
 }
