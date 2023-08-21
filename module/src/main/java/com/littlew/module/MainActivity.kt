4d1d59f3bbddee987da5ab0c0e685b7de9d3b704
Littlew <wei19960728@outlook.com>
2023-08-21 16:13:22 +0800

    add test module

---
+++ module/src/main/java/com/littlew/module/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.littlew.module
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.text_view)
+    }
+}
